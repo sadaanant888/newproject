6814d6e1e7b7a8b9e755a89c138c5defdcd25646
nikhil25335 <nikhil25335@iiitd.ac.in>
2026-03-08 11:46:34 +0530

    instruction r i s b

--- assembler.py
+++ assembler.py
@@ -102,9 +102,132 @@ for k in final:
         if(k[0] in instruction):
             ans+=instruction[k[0]][2] + "\n"
     
+    #I-TYPE
+    if(k[0] in i):
+        ans=""
+        # for lw rd,imm(rs1)
+        if k[0]==i[0]:
+            imm=k[2]                          ## k[2] is imm(rs1) in lw
+            num = int(imm.split("(")[0])      ## num is int(imm)
+            bin_imm=0,x=1                     ## bin_imm will store binary of immediate. Negative immediate not handled. 
+            while(num!=0):
+                rem=num/2
+                bin_imm=bin_imm+rem*x
+                num=num/2
+                x*=10
+            bin_imm=str(bin_imm)
+            bin_imm=bin_imm.rjust(12,'0') 
+            ans+=bin_imm+" "                           ## ans =imm
+
+            rs1=imm.split("(")[1].split(")")[0]
+            if rs1 in registers:
+                ans+=registers[rs1]+" "               ## ans= imm+ rs1
+            if k[0] in instruction:
+                ans+=instruction[k[0]][0]+" "         ## ans = imm + rs1+ funct3
+            if k[1] in registers:
+                ans+=registers[k[1]]+ " "                 ## ans = imm + rs1+ funct3+ rd
+            if k[0] in instruction:
+                ans+=instruction[k[0]][1]+ "\n"          ## ## ans = imm + rs1+ funct3+ rd+ opcode
+        
+        # except lw
+        else:
+            num = int(k[3])                   ## k[3] is imm
+            bin_imm=0,x=1                     ## bin_imm will store binary of immediate. Negative immediate not handled. 
+            while(num!=0):
+                rem=num/2
+                bin_imm=bin_imm+rem*x
+                num=num/2
+                x*=10
+            bin_imm=str(bin_imm)
+            bin_imm=bin_imm.rjust(12,'0') 
+            ans+=bin_imm+" "                           ## ans =imm
+
+            if k[2] in registers:
+                ans+=registers[k[2]]+" "               ## ans= imm+ rs1
+            if k[0] in instruction:
+                ans+=instruction[k[0]][0]+" "         ## ans = imm + rs1+ funct3
+            if k[1] in registers:
+                ans+=registers[k[1]]+ " "                 ## ans = imm + rs1+ funct3+ rd
+            if k[0] in instruction:
+                ans+=instruction[k[0]][1]+ "\n"
+
+    # S-TYPE
+    if(k[0] in s):
+        ans=""
+        # eg:- sw rs2,imm(rs1)
+        imm=k[2]                          ## k[2] is imm(rs1)
+        num = int(imm.split("(")[0])      ## num is int(imm)
+        bin_imm=0,x=1                     ## bin_imm will store binary of immediate. Negative immediate not handled. 
+        while(num!=0):
+            rem=num/2
+            bin_imm=bin_imm+rem*x
+            num=num/2
+            x*=10
+        bin_imm=str(bin_imm)
+        bin_imm=bin_imm.rjust(12,'0')
+
+        imm_11_5=""                       ## imm_11_5 will store imm[11:5]
+        y=0
+        while(y<7):
+            imm_11_5+=bin_imm[y]  
+            y+=1
+
+        ans+=imm_11_5+" "                           ## ans =imm[11:5]
+        if k[1] in registers:
+            ans+=registers[k[1]]+ " "              ## ans = imm[11:5] + rs2
+
+        rs1=imm.split("(")[1].split(")")[0]
+        if rs1 in registers:
+            ans+=registers[rs1]+" "                ## ans= imm[11:5]+ rs2+ rs1
+        if k[0] in instruction:
+            ans+=instruction[k[0]][0]+" "          ## ans = imm[11:5]+ rs2+ rs1+ funct3
+        
+        imm_4_0=""                                 ## imm_4_0 will store imm[4:0]
+        y=7
+        while(y<12):
+            imm_4_0+=bin_imm[y]
+            y+=1
+        ans+=imm_4_0+" "                            ## ans = imm[11:5]+ rs2+ rs1+ funct3+ imm[4:0]
+
+        if k[0] in instruction:
+            ans+=instruction[k[0]][1]+ "\n"        ## ans = imm[11:5]+ rs2+ rs1+ funct3+ imm[4:0]+ opcode
+    
+    # B-TYPE
+    if k[0] in b:
+        # eg:- beq rs1,rs2, imm[12:1]
+        ans=""
+        num=k[3]                          ## k[3] is imm
+        bin_imm=0,x=1                     ## bin_imm will store binary of immediate. Negative immediate not handled. 
+        while(num!=0):
+            rem=num/2
+            bin_imm=bin_imm+rem*x
+            num=num/2
+            x*=10
+        bin_imm=str(bin_imm)
+        bin_imm=bin_imm.rjust(16,'0')
+
+        imm_12__10_5=bin_imm[-13]+bin_imm[-11:-5]           #imm_12__10_5= imm[12|10:5]= imm[12]+imm[10:5]
+
+        ans+=imm_12__10_5+ " "           ## ans= imm[12|10:5]
+        
+        if k[2] in registers:
+            ans+=registers[k[2]]         ## ans= imm[12|10:5]+ rs2
+        if k[1] in registers:
+            ans+=registers[k[1]]         ## ans= imm[12|10:5]+ rs2 + rs1
+        if k[0] in instruction:
+            ans+=instruction[k[0]][0]    ## ans= imm[12|10:5]+ rs2 + rs1+ funct3
+        
+        imm_4_1__11=bin_imm[-5:-1]+ bin_imm[-12]    #imm_4_1__11= imm[4:1|11]= imm[4:1]+imm[11]
+        ans+=imm_4_1__11+ " "            ## ans= imm[12|10:5]+ rs2 + rs1+ funct3+ imm[4:1|11]
+
+        if k[0] in instruction:
+            ans+=instruction[k[0]][1]    ## ans= imm[12|10:5]+ rs2 + rs1+ funct3+ imm[4:1|11]+ opcode
+
+            
+    # U-TYPE
+    
+
+
 
     
 
-    
-
-# ab dbddslka
